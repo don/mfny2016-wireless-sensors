4103d375f7c8308844a81b5033299cfce4500fe5
Don Coleman <dcoleman@chariotsolutions.com>
2016-09-30 02:50:25 -0400

    import

---
+++ README.md
@@ -0,0 +1,7 @@
+Sample Code for Maker Faire NY 2016 Presentation
+
+Building Wireless Sensors
+
+http://makerfaire.com/maker/entry/58414/
+
+Slides: http://don.github.io/slides/2016-10-02-wireless-sensors
---
+++ Sensor/keys.h
@@ -0,0 +1,13 @@
+const char* ssid     = "dc";
+const char* password = "secret";
+
+#define HOST "data.sparkfun.com"
+int httpPort = 80;
+
+// You should create a new data stream at data.sparkfun.com
+// and replace these keys with your own keys
+// http://data.sparkfun.com/makerfaire
+#define PUBLIC_KEY "JxadlA4qllSNO6ovr6w7"
+#define PRIVATE_KEY "gz0ldm6vdduW0P6AoPVz"
+#define DELETE_KEY "eYx8ZvLKZ" 
+
